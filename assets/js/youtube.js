967af30817bc05b53701a405c075a3348cb78be9
root <root@DESKTOP-K560OBB.localdomain>
2020-04-12 04:16:20 +0900

    용서의 매커니즘 포스팅 추가

--- assets/js/youtube.js
+++ assets/js/youtube.js
@@ -1,6 +1,5 @@
 $(function() {
   $('.youtube').each(function(){
-    console.log("aaa")
     width = 560
     height = 315
     $(this).append("<iframe width=\""+width+"\" height=\""+height+"\" src=\"https://www.youtube.com/embed/"+$(this).attr('id')+"\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>");
