0d7460538e4d6c9e93a06adf13d0a5ada252617c
root <root@DESKTOP-K560OBB.localdomain>
2020-04-12 09:12:52 +0900

    youtube 영상 반응형으로 수정

--- assets/js/youtube.js
+++ assets/js/youtube.js
@@ -2,6 +2,6 @@ $(function() {
   $('.youtube').each(function(){
     width = 560
     height = 315
-    $(this).append("<iframe width=\""+width+"\" height=\""+height+"\" src=\"https://www.youtube.com/embed/"+$(this).attr('id')+"\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>");
+    $(this).append("<div style=\"position: relative; height:0; padding-bottom: 56.25%; margin: 40px 0px;\"><iframe width=\""+width+"\" height=\""+height+"\" src=\"https://www.youtube.com/embed/"+$(this).attr('id')+"?rel=0\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen style=\"position: absolute; width:100%; height:100%;\"></iframe></div>");
   });
 });
